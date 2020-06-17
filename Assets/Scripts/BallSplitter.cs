a5aa96f245f8fea7fad05f34b3214f7b96136e84
Paul Forest <paulf@forestgames.ca>
2020-06-17 13:54:44 -0400

    make a hello world model created with ProTools importing TMP assets create ball class create splitter class and defining a cheap way of identifying if a ball can hit the same ball splitter again

---
+++ Assets/Scripts/BallSplitter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Specialized;
+using UnityEngine;
+
+public class BallSplitter : MonoBehaviour
+{
+    [Header("How many balls will this split into?")] [SerializeField]
+    protected int mSplitCount;
+
+    private int _myBitFieldMask;
+
+    private static int _lastBitFieldMask;
+
+    private void Start()
+    {
+        _myBitFieldMask = BitVector32.CreateMask(_lastBitFieldMask);
+        _lastBitFieldMask = _myBitFieldMask;
+
+        Debug.Log($"myBitFieldMask={_myBitFieldMask}");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerBall ball = other.GetComponent<PlayerBall>();
+        if (!ball) return;
+
+        if (!CanCollideWithBall(ball)) return;
+
+        DoTheSplits(ball);
+    }
+
+    private void DoTheSplits(PlayerBall ball)
+    {
+        SetCannotCollideWithPlayerBall(ball);
+
+        for (int i = 0; i < mSplitCount; i++)
+        {
+            GameObject go = GameObject.Instantiate(ball.gameObject);
+            SetCannotCollideWithPlayerBall(go.GetComponent<PlayerBall>());
+        }
+    }
+
+    public bool CanCollideWithBall(PlayerBall ball)
+    {
+        return (ball.mSplittersUsedBitfield & _myBitFieldMask) == 0;
+    }
+
+    private void SetCannotCollideWithPlayerBall(PlayerBall ball)
+    {
+        ball.mSplittersUsedBitfield |= _myBitFieldMask;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
