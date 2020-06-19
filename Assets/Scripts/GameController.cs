50cf95ba0e1b74eca7a8c80618c7dc2eeea11e3f
Paul Forest <paulf@forestgames.ca>
2020-06-19 02:56:11 -0400

    WIP

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private readonly Dictionary<string, Scene> _gameLevels = new Dictionary<string, Scene>();
+    public Player Player { get; private set; }
+
+    public static GameController Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        _instance = this;
+
+        var max = SceneManager.sceneCountInBuildSettings;
+        for (var i = 0; i < max; i++)
+        {
+            var scene = SceneManager.GetSceneByBuildIndex(i);
+            if (scene.name.ToLower().Contains("level") || scene.path.ToLower().Contains("level"))
+            {
+                _instance._gameLevels.Add(scene.name, scene);
+                Debug.Log($"{scene.path}");
+            }
+        }
+    }
+
+    private static GameController _instance;
+
+    private void Start()
+    {
+        Player = Player.Instance;
+        if (string.IsNullOrEmpty(Player.LastLevelPlayed))
+        {
+            // Player.LastLevelPlayed = _gameLevels.;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player
+{
+    private const string LastLevelKey = "LastLevelPlayed";
+
+    public static Player Instance
+    {
+        get => Instance ?? (Instance = new Player());
+        private set => Instance = value;
+    }
+
+    public string LastLevelPlayed { get; set; }
+
+    private Player()
+    {
+        if (PlayerPrefs.HasKey(LastLevelKey))
+        {
+            // Get all data from player prefs
+            LastLevelPlayed = PlayerPrefs.GetString(LastLevelKey);
+        }
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetString(LastLevelKey, LastLevelPlayed);
+        PlayerPrefs.Save();
+    }
+}
