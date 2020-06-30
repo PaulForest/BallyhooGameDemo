fbd9c280d316ffd2c47cf84aae4497a40eb4ea05
Paul Forest <paulf@forestgames.ca>
2020-06-30 16:06:47 -0400

    WIP adding level 2

---
+++ Assets/Scripts/PlayObjects/RotateMe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PlayObjects
+{
+    public class RotateMe : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotationAxis = Vector3.forward;
+        [SerializeField] private float rotateRate = 0.1f;
+
+        private Transform _myTransform;
+
+        public void Start()
+        {
+            _myTransform = GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+            _myTransform.Rotate(rotationAxis * (rotateRate * Time.deltaTime));
+        }
+    }
+}
