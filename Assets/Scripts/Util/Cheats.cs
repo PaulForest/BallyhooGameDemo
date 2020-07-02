7a4c0aa2b452612279b531a525beb83369fd292c
Paul Forest <paulf@forestgames.ca>
2020-07-02 16:40:27 -0400

    adding quick n dirty cheat to reset the game via the R key

---
+++ Assets/Scripts/Util/Cheats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Util
+{
+    public class Cheats : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
